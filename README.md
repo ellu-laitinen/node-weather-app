f2077dc606148dec54edb08ffefe04ac22b8de0f
ElluLaitinen <ellu.laitinen85@gmail.com>
2020-08-26 19:28:53 +0300

    add readme

---
+++ README.md
@@ -0,0 +1,8 @@
+Weather App
+
+Uses data from mapbox.com and weatherstack.com
+
+from Udemy.com course The Complete Node.js Developer(3rd Edition) by Andrew Mead
+
+![Weather page](weather-app1.png)
+![Weather page 2](weather-app2.png)
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,13 +1,14 @@
-const request  = require('request')
+const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiZWxsdWxhaXQiLCJhIjoiY2s4enY3bHg0MW4xNzNnb2s2OTE4aTRsYSJ9.62Frhn1PBWU4L5wDa_WnSA&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) +
+        '.json?access_token=pk.eyJ1IjoiZWxsdWxhaXQiLCJhIjoiY2s4enY3bHg0MW4xNzNnb2s2OTE4aTRsYSJ9.62Frhn1PBWU4L5wDa_WnSA&limit=1'
 
-    request({ url, json:true}, (error, {body}) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to Geocode service', undefined)
 
-        } else if (body.features.length === 0){
+        } else if (body.features.length === 0) {
             callback('No location found', undefined)
         } else {
             callback(undefined, {
